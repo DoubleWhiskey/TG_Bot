0d8dab5e0454b3816d890b7949c086caab08a68e
DoubleWhiskey <YPMPVUEzT9crs8Q>
2023-08-28 15:13:39 +0300

    hugger logic added

--- bot.py
+++ bot.py
@@ -12,11 +12,12 @@ bot = telebot.TeleBot(TOKEN)
 def say_hi(message):
     # Функция, отправляющая "Привет" в ответ на команду /start
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton('Удар!')
-    markup.add(item1)
+    kick = types.KeyboardButton('Удар!')
+    hug = types.KeyboardButton('Обнимашки!')
+    markup.add(kick, hug)
     bot.send_message(message.chat.id, 'Привет')
     bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAALWxmTnBU8qge-uoBMFtAvw10YamWIkAAK2CQACeVziCcZOco3KlyVIMAQ')
-    bot.send_message(message.chat.id, 'Хочешь кого-нибудь ударить?', reply_markup=markup)
+    bot.send_message(message.chat.id, 'Хочешь кого-нибудь ударить или обнять?', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['voice'])
@@ -27,11 +28,13 @@ def transcript(message):
     bot.reply_to(message, text)
 
 
-def kick_ass(kicker):
-    users_list = ['ТайныйКраб', 'ToribezB', 'Belkakel', 'K_tsx', 'Vasili', 'Keks9tina', 'Bilk123']
+users_list = ['ТайныйКраб', 'ToribezB', 'Belkakel', 'K_tsx', 'Vasili', 'Keks9tina', 'Bilk123']
 
+
+def kick_ass(kicker):
     victim = choice(list(filter(lambda x: x != kicker, users_list)))
     amount = randint(1, 32)
+
     if amount in (1, 21, 31):
         teeth = 'зуб'
     elif amount % 10 in (2, 3, 4) and str(amount)[0] != '1':
@@ -46,12 +49,30 @@ def kick_ass(kicker):
     return f"{kicker} {kicker_gender} @{victim} и {result} {victim_gender} {amount} {teeth}!"
 
 
+def hug_func(hugger):
+    victim = choice(list(filter(lambda x: x != hugger, users_list)))
+    amount = randint(1, 666)
+    if amount % 10 == 1 and amount % 100 != 11:
+        flower = 'цветочек'
+    elif amount % 10 in (2, 3, 4) and str(amount)[-2] != '1':
+        flower = 'цветочка'
+    else:
+        flower = 'цветочков'
+
+    hugger_gender = 'обняла' if hugger in ('ToribezB', 'Belkakel') else 'обнял'
+    victim_gender = 'ей' if victim in ('ToribezB', 'Belkakel') else 'ему'
+    result = 'подарила' if hugger in ('ToribezB', 'Belkakel') else 'подарил'
+
+    return f"{hugger} {hugger_gender} @{victim} и {result} {victim_gender} {amount} {flower}!"
+
+
 @bot.message_handler(content_types=['text'])
 def echo(message):
     answers_dict = {
         'Привет': 'прив, чё делал?',
         'Как дела?': 'норм',
         'Удар!': kick_ass(message.from_user.username or message.from_user.first_name),
+        'Обнимашки!': hug_func(message.from_user.username or message.from_user.first_name),
 
     }
     answer = answers_dict.get(message.text)
